681df59d1f36233852f4908927ba6ae0787db917
Salaxy <Salaxy@web.de>
2011-11-07 20:41:17 +0100

    Sync mit Minke

--- TwitterHausaufgabe/src/Start.java
+++ TwitterHausaufgabe/src/Start.java
@@ -1,25 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Timer;
+
+
 
 
 public class Start {
-
 	
+	static Timer timer = new Timer();
+
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {	
-		
+	public static void main(String[] args){	
 		//Twitterbot erstellen
-		TwitterBot bot=new TwitterBot();
-		
+		TwitterBot bot= new TwitterBot();		
 		//anmelden des eigenen Accounts
-		bot.login();
-		
-		//Kontakte adden
-		bot.addToFollow("twitt3rbot");
-		bot.addToFollow("TwitBot2");
-		
-		//Thread starten
-		bot.run();
+		bot.login();		
+		timer.schedule(bot, 0, 5000);	
+		menue(bot);
+	}
+	
+	
+	/**
+	 * Zeigt das Menue an
+	 */
+	private static void showMenue(){
+		System.out.println("-----------------------------------------------------------------");
+		System.out.println("-----------Drücke 1 zum Folgen eines Users-----------------------");
+		System.out.println("-----------Drücke 2 zum erneuten anzeigen des Menues. -----------");
+		System.out.println("-----------Drücke 3 zum Schließen des Programms.-----------------");
+		System.out.println("-----------Die Auswahl mit Enter bestätigen.---------------------");
+		System.out.println("-----------------------------------------------------------------");
+	}
+	
+	
+	/**
+	 * Einlesen und auswerten der Benutzereingabe
+	 * 
+	 * @param bot
+	 */
+	private static void menue(TwitterBot bot){
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+		showMenue();
+		while(true){
+			try {
+				int auswahl = Integer.parseInt(br.readLine());
+				switch(auswahl){
+				case 1: System.out.println("Geben Sie den Names des zu Folgenden Users ein(@Username)");
+					    bot.followTwitterUser(br.readLine());
+					    showMenue();
+						break;
+				case 2: showMenue();
+						break;
+				case 3: System.exit(0);
+						break;
+				default:
+					System.out.println("Falsche Eingabe");
+				}				
+			} catch (NumberFormatException e) { 
+			} catch (IOException e) {
+			} 
+		}
 	}
 
 }
--- TwitterHausaufgabe/src/TwitterBot.java
+++ TwitterHausaufgabe/src/TwitterBot.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TimerTask;
 
 import twitter4j.Status;
 import twitter4j.Twitter;
@@ -7,24 +8,26 @@ import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
-public class TwitterBot extends Thread{
+public class TwitterBot extends TimerTask  {
 
 	protected ArrayList<Long> ids = new ArrayList<Long>();
-	
-	private static String CON_KEY = "LsdtBCRJN3lSotARzeuQYw";
-	private static String CON_SEC = "0ylBJAWGfN5ZJe5V0vWbNbKS8d9ZZOvwWNuT3mtrQ";
-	private static String ACC_KEY = "406540577-Mpn45CROCW47IZ7XtSiiluah7hmMKHiHxRPYzX8A";
-	private static String ACC_SEC = "J2S9avix6F3bJTMv2BeDMkiax3Hsibe716ZhmHaAgk";
-	
-	private static String STANDARD_ANSWER="Ich bin nur ein dummer Twitterbot123!";	
-	
+
+	private static String CON_KEY = "yZR1qUww5XUo7AyGULMAcw";
+	private static String CON_SEC = "JlzcAihS1C8fIA6rUJNBTr4ucVHUt3uYGkgLzZ6A";
+	private static String ACC_KEY = "404279119-gO2LepPJrIMB4mXJnyfjVoZdugHWFT6tooIU5qE";
+	private static String ACC_SEC = "gimlxVg1S0m1W7cEDOPyDzF5Mn0dY9pr9ZbBh9iM3Zk";
+
+	private static String STANDARD_ANSWER = " The person you have talked is temporarily not available";
+
 	private Twitter twitter;
-	
 
 	public TwitterBot() {
 
 	}
 
+	/**
+	 * Authentifiziert die Applikation mit Twitter
+	 */
 	public void login() {
 		ConfigurationBuilder cb = new ConfigurationBuilder();
 		cb.setDebugEnabled(true);
@@ -32,53 +35,48 @@ public class TwitterBot extends Thread{
 		cb.setOAuthConsumerSecret(CON_SEC);
 		cb.setOAuthAccessToken(ACC_KEY);
 		cb.setOAuthAccessTokenSecret(ACC_SEC);
-		
+
 		TwitterFactory tf = new TwitterFactory(cb.build());
 		this.twitter = tf.getInstance();
 	}
 
-
 	public void addToFollow(String name) {
-		
+
 		this.followTwitterUser(name);
 	}
 
-	
-	public void run(){
-		
+	/**
+	 * wird alle 5 Sekunden aufgerufen
+	 */
+	public void run() {
 		this.checkToAnswer();
-		try {
-			//max 120 Anfragen/Stunde
-			TwitterBot.sleep(30000l);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
 	}
 
 	/**
-	 * checke Kontakte ab und antworte falls nötig
+	 * checke Kontakte ab und antworte falls nötig.
 	 */
 	public void checkToAnswer() {
+
 		
-		List<Status> stats;
 		try {
-			stats = twitter.getMentions();		
-			
+			List<Status> stats;
+			stats = twitter.getMentions();
+				
 			for (Status status : stats) {
 				if (isNotYetAnswered(ids, status.getId())) {
-					update("@" + status.getUser().getScreenName() + " sent: " + STANDARD_ANSWER);
+					 update("@" + status.getUser().getScreenName() + STANDARD_ANSWER);
 				}
 			}
-		} catch (TwitterException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} catch (TwitterException te) {
 		}
-
 	}
-	
-	
+
+	/**
+	 * Setzt angegegeben User auf Folgen.
+	 * 
+	 * @param name 
+	 * 			(Name des zu folgenden Users )
+	 */
 	public void followTwitterUser(String name) {
 
 		try {
@@ -88,8 +86,15 @@ public class TwitterBot extends Thread{
 			System.err.println("Konnte nicht folgen: " + te.getMessage());
 		}
 	}
-	
-	
+
+	/**
+	 * 
+	 * Überprüft ob die auf eine Nachricht schonmal geantwortet wurde.
+	 * @param ids
+	 * @param id
+	 * @return
+	 * 		(true, wenn schon auf die Nachricht geantwortet wurde)
+	 */
 	public boolean isNotYetAnswered(List<Long> ids, long id) {
 
 		if (ids.contains(id)) {
@@ -99,14 +104,21 @@ public class TwitterBot extends Thread{
 			return true;
 		}
 	}
+
 	
+	/**
+	 * setzt einen neuen Status des Twitteraccounts
+	 * 
+	 * @param message
+	 *        (Nachricht die im geschrieben werden soll)
+	 */	
 	public void update(String message) {
 
 		try {
 			Status status = twitter.updateStatus(message);
-			System.out.println("Erfolgreich updated zu (" + status.getText() + ")");
-		} catch (TwitterException te) {
-			 System.err.println("Konnte nicht updaten: " + te.getMessage());
+			System.out.println("Erfolgreich updated zu (" + status.getText()
+					+ ")");
+		} catch (TwitterException te) {	
 		}
 	}
 }
