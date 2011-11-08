d8a7f4de3e7fdfc0c799e96382f7899d63425ed7
Salaxy <Salaxy@web.de>
2011-11-08 12:00:27 +0100

    package erstellt

--- TwitterHausaufgabe/src/de/fhb/twitterbot/Start.java
+++ TwitterHausaufgabe/src/de/fhb/twitterbot/Start.java
@@ -1,3 +1,4 @@
+package de.fhb.twitterbot;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
--- TwitterHausaufgabe/src/de/fhb/twitterbot/TwitterBot.java
+++ TwitterHausaufgabe/src/de/fhb/twitterbot/TwitterBot.java
@@ -1,3 +1,4 @@
+package de.fhb.twitterbot;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.TimerTask;
